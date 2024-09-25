d3b404245b1ef766b1aeb5865e1b15afa6058926
Deanber <2511688525@qq.com>
2024-09-25 16:24:55 +0800

    Update Message.cs

--- Server/Editor/Message/Message.cs
+++ Server/Editor/Message/Message.cs
@@ -4,16 +4,27 @@ namespace Editor
 {
     class Message
     {
-        public string receive(int Header,byte[] message)
+        //接收消息，处理消息
+        public string receive(byte[] message)
         {
-            int HeaderLength=4;
+            Int32 HeaderLength;
+            
             string data=System.Text.Encoding.UTF8.GetString(message);
             
             return data;
         }
-        public byte[] send(int Header,string data)
+
+        //传入数据头数据和数据体数据
+        public byte[] send(Int32 Header,byte[] data)
         {
+            //数据头
             byte[] message=new byte[1024];
+            byte[] HeaderLength=BitConverter.GetBytes(Header);
+            if(BitConverter.IsLittleEndian)
+                Array.Reverse(HeaderLength);
+            message=HeaderLength.Concat(data).ToArray();
+            //添加分隔符
+            byte[] delimiterbytes=System.Text.Encoding.UTF8.GetBytes("\n");
             return message;
         }
     }
